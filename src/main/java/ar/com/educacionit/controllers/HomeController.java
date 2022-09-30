1527a985ac12c089f20d553075684e42bd5bff41
Rodrgio Pastrana Lopez <rodrigopastra@gmail.com>
2022-09-29 23:24:23 -0300

    ejercicio 12092022

---
+++ src/main/java/ar/com/educacionit/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package ar.com.educacionit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value = "/")
+	public String home () {
+		
+		return "index";
+	}
+	
+	
+	
+}
